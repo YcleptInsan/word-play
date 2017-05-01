ab91f30d05b0049f8c9f5fb663e8207b1f123837
david rolfs and nick wise <david@>
2017-05-01 14:41:40 -0700

    update map to use filter loop to fileter out words less then three charachters then reverse and join back together into a string

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,12 @@ $(document).ready(function(){
     var words = $("#words").val().split(" ");
     // console.log(words)
 
+    var longerWords = words.filter(function(word){
+      return word.length > 3
+    });
+
+    longerWords.reverse()
+
+    $("ul").append(longerWords.join(" "));
   });
 });
